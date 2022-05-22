18f29b3ca874e8e75a53cf1ea6d652824667cdfa
Shreyas Paliwal <shreyaspaliwal21@gmail.com>
2022-05-22 17:34:33 +0530

    Authentication - UserDetail

--- src/main/java/com/sheryians/major/model/User.java
+++ src/main/java/com/sheryians/major/model/User.java
@@ -52,8 +52,58 @@ public class User {
 	@ManyToMany(cascade=CascadeType.MERGE, fetch=FetchType.EAGER)
 	@JoinTable(
 			name="user_role", 
-			joinColumns= {@JoinColumn(name="USER_ID", referencedColumnName="ID")}, 
-			inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referenceColumnName="ID")}
+			joinColumns = {@JoinColumn(name="USER_ID", referencedColumnName="ID")}, 
+			inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referencedColumnName="ID")}
 			)
 	private List<Role> roles;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	
 }
