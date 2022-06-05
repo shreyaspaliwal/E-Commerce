f588c96b3ff0739d737359fa86e09dc19c234e3f
Shreyas Paliwal <shreyaspaliwal21@gmail.com>
2022-06-05 22:28:15 +0530

    Added Procfile for Heroku deployment

--- src/main/java/com/sheryians/major/MajorApplication.java
+++ src/main/java/com/sheryians/major/MajorApplication.java
@@ -1,13 +1,27 @@
 package com.sheryians.major;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MajorApplication {
+	
+	//private static Logger log = LoggerFactory.getLogger(MajorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MajorApplication.class, args);
 	}
+	
+	/*@Value("${spring.security.oauth2.client.registration.google.client-id}")
+	private String clientId;
+	
+	@Value("${spring.security.oauth2.client.registration.google.client-secret}")
+	private String clientSecret;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("message");
+	}*/
 
 }
